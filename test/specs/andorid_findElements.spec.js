3bfbcb07f0d9edb10dde51547a901a323330a35e
Weber Lucas <weber13lucas@gmail.com>
2026-03-18 19:41:52 -0300

    adicionando teste de class e id

--- test/specs/andorid_findElements.spec.js
+++ test/specs/andorid_findElements.spec.js
@@ -1,14 +1,14 @@
 describe('Android Find Elements', () => {
-    it('Find element by accessibility id', async ()  => {
-    const appOption = await $('~App')
+    it('Find element by accessibility id', async () => {
+        const appOption = await $('~App')
 
-    await appOption.click()
-    const actionBar = await $('~Action Bar')
-    await expect(actionBar).toBeExisting();
+        await appOption.click()
+        const actionBar = await $('~Action Bar')
+        await expect(actionBar).toBeExisting();
     });
     it.only('Find element by class name', async () => {
-    const className = await $('.android.widget.TextView')
-    console.log(await className.getText())
-    await expect(className).toHaveText('API Demos')
+        const className = await $('.android.widget.TextView')
+        console.log(await className.getText())
+        await expect(className).toHaveText('API Demos')
     })
 });
