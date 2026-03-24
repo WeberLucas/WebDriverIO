6390f9b3ab6fba7c0183735c028d20dbb831eee1
Weber Lucas <weber13lucas@gmail.com>
2026-03-23 21:59:32 -0300

    xpath

--- test/specs/andorid_findElements.spec.js
+++ test/specs/andorid_findElements.spec.js
@@ -6,9 +6,19 @@ describe('Android Find Elements', () => {
         const actionBar = await $('~Action Bar')
         await expect(actionBar).toBeExisting();
     });
-    it.only('Find element by class name', async () => {
+    it('Find element by class name', async () => {
         const className = await $('.android.widget.TextView')
         console.log(await className.getText())
         await expect(className).toHaveText('API Demos')
+    })
+    it('Find element by xpath', async () => {
+     await $('//android.widget.TextView[@content-desc="Alert Dialogs"]').click()
+        
+     await $('//android.widget.Button[@resource-id="io.appium.android.apis:id/select_button"]').click()
+
+     await $('//android.widget.TextView[@text="Command two"]').click()
+
+     const textAssertion = await $('//android.widget.TextView')
+     await expect(textAssertion).toHaveText('You selected: 1 , Command two')
     })
 });
