1f67373f85b0c561b61e3de330bde60766596280
Weber Lucas <weber13lucas@gmail.com>
2026-05-16 16:37:28 -0300

    Scrollando ate encontrar um elemento no android

--- test/specs/andorid_findElements.spec.js
+++ test/specs/andorid_findElements.spec.js
@@ -1,45 +1,59 @@
 describe('Android Find Elements', () => {
-    it('Find element by accessibility id', async () => {
-        const appOption = await $('~App')
+  it('Find element by accessibility id', async () => {
+    const appOption = await $('~App')
 
-        await appOption.click()
-        const actionBar = await $('~Action Bar')
-        await expect(actionBar).toBeExisting();
-    });
-    it('Find element by class name', async () => {
-        const className = await $('.android.widget.TextView')
-        console.log(await className.getText())
-        await expect(className).toHaveText('API Demos')
-    })
-    xit('Find element by xpath', async () => {
-     await $('//android.widget.TextView[@content-desc="Alert Dialogs"]').click()
-        
-     await $('//android.widget.Button[@resource-id="io.appium.android.apis:id/select_button"]').click()
+    await appOption.click()
+    const actionBar = await $('~Action Bar')
+    await expect(actionBar).toBeExisting();
+  });
+  it('Find element by class name', async () => {
+    const className = await $('.android.widget.TextView')
+    console.log(await className.getText())
+    await expect(className).toHaveText('API Demos')
+  })
+  xit('Find element by xpath', async () => {
+    await $('//android.widget.TextView[@content-desc="Alert Dialogs"]').click()
 
-     await $('//android.widget.TextView[@text="Command two"]').click()
+    await $('//android.widget.Button[@resource-id="io.appium.android.apis:id/select_button"]').click()
 
-     const textAssertion = await $('//android.widget.TextView')
-     await expect(textAssertion).toHaveText('You selected: 1 , Command two')
-    })
-    it('Find element by UiAutomator', async () => {
-        await $('android=new UiSelector().textContains("Alert")').click()
-    })
+    await $('//android.widget.TextView[@text="Command two"]').click()
 
-    it.only('Find multiple elements', async () => {
-      const expectedList=[
-        'API Demos', "Access'ibility",
-        'Accessibility','Animation',
-        'App','Content','Graphics',
-        'Media','NFC','OS',
-        'Preference','Text','Views'
-      ]
+    const textAssertion = await $('//android.widget.TextView')
+    await expect(textAssertion).toHaveText('You selected: 1 , Command two')
+  })
+  it('Find element by UiAutomator', async () => {
+    await $('android=new UiSelector().textContains("Alert")').click()
+  })
 
-      const actualList=[]
+  it('Find multiple elements', async () => {
+    const expectedList = [
+      'API Demos', "Access'ibility",
+      'Accessibility', 'Animation',
+      'App', 'Content', 'Graphics',
+      'Media', 'NFC', 'OS',
+      'Preference', 'Text', 'Views'
+    ]
 
-      const textList =await $$('android.widget.TextView')
-      for(const element of textList){
-        actualList.push(await element.getText())      
-      }
-      await expect(actualList).toEqual(expectedList)
-    })
+    const actualList = []
+
+    const textList = await $$('android.widget.TextView')
+    for (const element of textList) {
+      actualList.push(await element.getText())
+    }
+    await expect(actualList).toEqual(expectedList)
+  })
+  it.only('Vertical srolling', async () => {
+    await $('~App').click()
+    await $('~Activity').click()
+
+    //scrola até o fim sem olhar nada
+    //await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrolltOEnd(1,5)')
+    //scrolla procurando o elemento de texto secure surfaces
+    await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollTextIntoView("Secure Surfaces"))').click()
+
+    await expect($('~Secure Surfaces')).toExist()
+
+
+
+  })
 });
